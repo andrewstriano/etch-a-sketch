a1f2c106a1e535b1143787d2edaabe8c4c1cd2b6
Andrew Striano <andrewstriano@gmail.com>
2023-02-01 08:50:37 -0800

    create basic functionality, still has some bugs
    everything works for the basic 16 x 16 grid.
    The reset button clears the page and populates a new grid, the
    dimensions are stick at 16 tiles wide.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # etch-a-sketch
 etch-a-sketch game made with JavaScript. 
+
+This game will meet the following requirements: 
+1. 16 x 16 grid of divs that have no space between them and they fill up a square container.
+2. a button that launches a pop up
+2. a.) this pop up with prompt the user to input desired number of divs in each row/column.
+2. b.) this will launch a new array of divs that will replace the original one BUT will take up the same area.
+3. this grid of divs will change color as you hover over them leaving a trail behind the cursor.
+
--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+const gameContainer = document.querySelector("#game-container");
+const resetButton = document.querySelector("#reset-button");
+let height = 16;
+let width = 16;
+function buildGrid(height, width) { // builds the grid based on two parameters (height and width).
+    for (let i = 1; i < ((height * width)+1); i++) {
+        const div = document.createElement("div");
+        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;";
+        gameContainer.appendChild(div);
+        div.classList.add("grid-items");
+    };    
+};
+buildGrid(height, width); // builds initial grid
+
+let divs = document.querySelectorAll(".grid-items");
+for (let i=0;i<divs.length;i++) {
+    divs[i].addEventListener("mouseover", function changeColor() {
+        divs[i].classList.add("change-color");
+    }) 
+}
+function reset() { // reset function to replace grid with user defined new one
+    height = prompt("Enter desired height");
+    width = prompt("Enter desired width");
+    gameContainer.replaceChildren();
+    buildGrid(height, width);
+    gameContainer.style.width = width
+}
+
+
+resetButton.addEventListener("click", reset);
+
+
+
+
 
